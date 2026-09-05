21441bca921281341df108875485c09529743b38
honore <honoreniyogushimwa63@gmail.com>
2026-09-05 09:41:31 +0200

    refactor

---
+++ .next/static/chunks/app/youth/progress/page-72df4d7ab9b0ace3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8124],{7215:(e,s,_)=>{Promise.resolve().then(_.bind(_,8982))}},e=>{e.O(0,[505,2619,8982,8441,1255,7358],()=>e(e.s=7215)),_N_E=e.O()}]);
